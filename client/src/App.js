4ad184b9c235f9e8637bb532c2a369c1a01cba63
VedangKanade <vedangk20@gmail.com>
2024-01-15 21:28:47 +0530

    made more changes to bg

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
-import "./App.css";
 
 function App() {
   const mode = useSelector((state) => state.mode);
